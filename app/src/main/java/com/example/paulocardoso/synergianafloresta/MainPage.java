7f5e7505b204754e72640b04628dc415921a8a0f
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2018-03-09 13:42:23 +0000

    added some content

--- app/src/main/java/com/example/paulocardoso/synergianafloresta/MainPage.java
+++ app/src/main/java/com/example/paulocardoso/synergianafloresta/MainPage.java
@@ -1,12 +1,10 @@
 package com.example.paulocardoso.synergianafloresta;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 import android.view.MenuItem;
 
 public class MainPage extends AppCompatActivity {
@@ -17,15 +15,9 @@ public class MainPage extends AppCompatActivity {
         setContentView(R.layout.activity_main_page);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        TextView descriptionText = (TextView) findViewById(R.id.descriptionText);
+        descriptionText.setText("Synergia na Floresta");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
